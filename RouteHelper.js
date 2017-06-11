e52191f88c56d1f28104e447abe21e3d68a0cdd1
Nasir Billoo <nasir.billoo@gmail.com>
2017-06-11 14:15:38 -0400

    updates

--- RouteHelper.js
+++ RouteHelper.js
@@ -12,7 +12,11 @@ RouteHelper.prototype = {
             message: err
         });
     },
-
+    authError: function (message, res) {
+        res.status(403).send({
+            message: message
+        });
+    },
     sendAuthError: function (res, message) {
         res.status(403).send({
             message: message
@@ -207,31 +211,6 @@ RouteHelper.prototype = {
             n = 100;
         return val && typeof val === "string" ? this.sequalizeString(val).substring(0, n) : null;
     },
-    mysqlEscapeString: function (str) {
-        return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
-            switch (char) {
-                case "\0":
-                    return "\\0";
-                case "\x08":
-                    return "\\b";
-                case "\x09":
-                    return "\\t";
-                case "\x1a":
-                    return "\\z";
-                case "\n":
-                    return "\\n";
-                case "\r":
-                    return "\\r";
-                case "\"":
-                case "'":
-                case "\\":
-                case "%":
-                    return "\\" + char; // prepends a backslash to backslash, percent,
-                // and double/single quotes
-            }
-        });
-    },
-    
 }
 
 var routeHelper = new RouteHelper();
